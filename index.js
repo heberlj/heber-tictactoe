f28082eb14851a629927027a734a1e6ee0a4e391
HeberLJ <hlopezjose2000@gmail.com>
2026-06-17 22:09:44 -0400

    Style: Pantalla de carga

--- index.js
+++
@@ -1,8 +0,0 @@
-console.clear();
-console.log("Hello world");
-
-
-function mostrarPantalla(nombrePantalla) {
-    document.querySelectorAll(".pantalla").style.display = "none";
-    document.querySelector(nombrePantalla).style.display = "block";
-}
